feefd4163d2260a0c5d27ab35e3d546043e8445c
Paulo Vitor <paulo19032004@gmail.com>
2021-10-03 22:04:02 -0300

    Materalize, Discount feature and style.css Add

--- main.js
+++ main.js
@@ -1,47 +1,35 @@
 const submit = document.querySelector('#submit')
 const resultFee = document.querySelector('#result-fee')
 const resultProfLoss = document.querySelector('#result-profLoss')
-const discount50 = document.querySelector('#off50')
-const minProfit = 0.50
+const discountText = document.querySelector('#result-porcent')
+const discountTextResult = document.querySelector('#porcentDisplay')
+const minProfit = 0.5
 
-
-
-function discount50Function (fabValue) {
+function discount(fabValue, porcent) {
   let recomendationValue = 0
   let realValue = 0
-  
+
   while (realValue <= minProfit) {
     recomendationValue += 0.1
-    let fee50Discount = (6.99 / 100) * recomendationValue + 0.39
-    let discountedValue = recomendationValue - (recomendationValue * 0.5)
-    realValue = discountedValue - fee50Discount - fabValue
+    let feeDiscount = (6.99 / 100) * recomendationValue + 0.39
+    let discountedValue =
+      recomendationValue - recomendationValue * (porcent / 100)
+    realValue = discountedValue - feeDiscount - fabValue
     console.log(realValue, discountedValue, recomendationValue)
   }
   return recomendationValue
 }
 
-
 submit.addEventListener('click', function () {
   const fabValue = parseFloat(document.querySelector('#price-fab').value)
   const storeValue = parseFloat(document.querySelector('#price-store').value)
+  const discountValue = parseFloat(document.querySelector('#porcent').value)
   const fee = (6.99 / 100) * storeValue + 0.39
   const liquidValue = storeValue - fee - fabValue
-  const recomendPrice = discount50Function(fabValue, fee)
+  const recomendPrice = discount(fabValue, discountValue)
 
   resultFee.innerHTML = fee
   resultProfLoss.innerHTML = liquidValue
-  discount50.innerHTML = recomendPrice
-
-  // if (liquidValue < 0) {
-  //   resultMaxDiscount.innerHTML = 'No Discount Aplicable'
-  // } else {
-  //   discout = 0
-  //   liquidValueDiscount = liquidValue
-  //   while (liquidValueDiscount >= 0) {
-  //     liquidValueDiscount = liquidValue - (discout / 100) * liquidValue
-  //     discout += 1
-  //   }
-  //   console.log(discout)
-  //   resultMaxDiscount.innerHTML = `${discout} %`
-  // }
+  discountText.innerHTML = `O Valor Recomendado pra um desconto de ${discountValue}%`
+  discountTextResult.innerHTML = recomendPrice
 })

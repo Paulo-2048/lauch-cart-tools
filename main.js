6c22398af738fb38c374dc24ac5f62055fdf0fb9
Paulo Vitor <paulo19032004@gmail.com>
2021-10-07 08:44:56 -0300

    Porcent Feature Add

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const resultFee = document.querySelector('#result-fee')
 const resultProfLoss = document.querySelector('#result-profLoss')
 const discountText = document.querySelector('#result-porcent')
 const discountTextResult = document.querySelector('#porcentDisplay')
+const maxDiscountResult = document.querySelector('#maxDiscount')
 const minProfit = 0.5
 
 function discount(fabValue, porcent) {
@@ -20,6 +21,21 @@ function discount(fabValue, porcent) {
   return recomendationValue
 }
 
+function maxDiscount(fabValue, storeValue, fee) {
+  let porcentValue = 0
+  let maxValueDesconted = 1
+
+  while (maxValueDesconted >= minProfit) {
+    porcentValue += 0.5
+    maxValueDesconted =
+      storeValue - storeValue * (porcentValue / 100) - fee - fabValue
+    console.log(porcentValue, maxValueDesconted)
+  }
+  return porcentValue
+}
+
+// (storeValue - storeValue * (porcentValue / 100)) - fee - fabValue
+
 submit.addEventListener('click', function () {
   const fabValue = parseFloat(document.querySelector('#price-fab').value)
   const storeValue = parseFloat(document.querySelector('#price-store').value)
@@ -27,9 +43,11 @@ submit.addEventListener('click', function () {
   const fee = (6.99 / 100) * storeValue + 0.39
   const liquidValue = storeValue - fee - fabValue
   const recomendPrice = discount(fabValue, discountValue)
+  const priceMaxDiscount = maxDiscount(fabValue, storeValue, fee)
 
   resultFee.innerHTML = fee
   resultProfLoss.innerHTML = liquidValue
-  discountText.innerHTML = `O Valor Recomendado pra um desconto de ${discountValue}%`
+  discountText.innerHTML = `O Valor Recomendado pra um desconto de ${discountValue}%:`
   discountTextResult.innerHTML = recomendPrice
+  maxDiscountResult.innerHTML = priceMaxDiscount
 })
